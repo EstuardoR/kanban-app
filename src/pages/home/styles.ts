01fcccfd7aa6bc78700e549695bafcaf64c06822
Joan Rodas <joan.rodas@multigroup.com>
2026-07-07 16:40:00 -0600

    refactor: improve layout and styling of main and column components

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -4,29 +4,45 @@ interface ColumnStyledProps {
     bg: string
 }
 
-
 export const MainWrapperStyled = styled.div`
- width: 100vw;
- height: 100vh;
- background-color: white;
- color: black;
- position: absolute;
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+
+  background: white;
+  color: black;
 `;
 
 
 export const ColumnsContainerStyled = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    gap: 10px;
-    height: 70%;
+  display: flex;
+  flex: 1;
+
+  gap: 30px;
+  width: 100%;
+
+  min-height: 0;
 `;
 
+
 export const ColumnStyled = styled.div.withConfig({
-    shouldForwardProp: (prop) => prop !== "bg",
-}) <ColumnStyledProps>`
-    width: 25%;
-    height: 100%;
-   background-color: ${(props) => props.bg};
-`
+  shouldForwardProp: (prop) => prop !== "bg",
+})<ColumnStyledProps>`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+
+  padding: 10px;
+  gap: 10px;
+
+  overflow-y: auto;
+  min-height: 0;
+
+  justify-content: flex-start;
+  align-items: center;
+
+  background: ${(props) => props.bg};
+`;
