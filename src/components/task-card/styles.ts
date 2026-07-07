d6a43bbe4e7cf633ba38a9f0d588c164a774867e
Joan Rodas <joan.rodas@multigroup.com>
2026-07-07 16:39:37 -0600

    feature: enhance task card styles

--- src/components/task-card/styles.ts
+++ src/components/task-card/styles.ts
@@ -1,7 +1,24 @@
-import { Box, styled } from "@mui/material";
+import { Box, styled, Typography } from "@mui/material";
 
 
 export const CardTaskContainerStyled = styled(Box)(() => ({
-    
-    
+    width: "90%",
+    display: "flex",
+    backgroundColor: "#ffffff",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "center",
+    borderRadius: "10px",
+    padding: "10px",
+}));
+
+export const CardTaskTitleStyled = styled(Typography)(() => ({
+    fontSize: "1.5rem",
+    fontWeight: "bold",
+    marginBottom: "10px",
+}));
+
+export const CardTaskDescriptionStyled = styled(Typography)(() => ({
+    fontSize: "1rem",
+    marginBottom: "10px",
 }));
