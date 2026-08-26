991b3c3fb3138c0e1acc63ad01aa9580d35fae85
Joan Estuardo <rrodasestuardo2004@gmail.com>
2026-08-25 23:40:19 -0600

    feature: implement dnd-kit

--- src/components/task-card/task-card.tsx
+++ src/components/task-card/task-card.tsx
@@ -5,14 +5,31 @@ import {
   CardTaskDescriptionStyled,
   CardTaskTitleStyled,
 } from "./styles";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 interface TaskCardProps {
   task: Task;
 }
 
 export const TaskCard = ({ task }: TaskCardProps): JSX.Element => {
+  const { id } = task;
+  const { listeners, attributes, setNodeRef, transform, transition } = useSortable({ id })
+
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+
+  }
+
   return (
-    <CardTaskContainerStyled>
+    <CardTaskContainerStyled
+      ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+      style={style}
+    >
       <CardTaskTitleStyled>{task?.title}</CardTaskTitleStyled>
       <CardTaskDescriptionStyled>{task?.description}</CardTaskDescriptionStyled>
     </CardTaskContainerStyled>
